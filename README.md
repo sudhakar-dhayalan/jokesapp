7c62b921d3681c67b5a14019eb326cc27119e55c
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2020-08-22 01:01:08 +0530

    jokes app initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Spring Boot Jokes App
+This application displays a random Chuck Norris joke on the index.
+ 
---
+++ src/test/java/code/master/jokesapp/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package code.master.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
